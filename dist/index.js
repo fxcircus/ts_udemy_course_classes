e50abaea95dc0c8f634e5dc639805de2311bdaa1
Roy Daniel <roydanielr@gmail.com>
2023-01-10 05:40:56 -0500

    classes final

--- dist/index.js
+++ dist/index.js
@@ -13,16 +13,41 @@
 //     }
 // }
 class Player {
-    constructor(first, last) {
+    constructor(first, last, _score, _region = "US") {
         this.first = first;
         this.last = last;
-        this.score = 0;
+        this._score = _score;
+        this._region = _region;
         this.secretMethod(); // This works! (we can access private from the constructor)
     }
     secretMethod() {
         console.log("Secret method!");
     }
+    // fullName will be set as readonly by default
+    get fullName() {
+        return `${this.first} ${this.last}`;
+    }
+    get score() {
+        return this._score;
+    }
+    set score(newScore) {
+        if (newScore < 0) {
+            throw new Error("Score cannot be negative");
+        }
+        this._score = newScore;
+    }
 }
-const player1 = new Player("Cooper", "dog");
+const player1 = new Player("Cooper", "dog", 0);
 console.log(player1);
 // player1.secretMethod() // Property 'secretMethod' is private and only accessible within class 'Player'.
+player1.score;
+// player1.score = -1 // Uncaught Error: Score cannot be negativee
+class SuperPlayer extends Player {
+    constructor() {
+        super(...arguments);
+        this.isAdmin = true;
+    }
+    switchToEuRegion() {
+        this._region = "EU";
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -15,17 +15,46 @@
 // }
 
 class Player {
-    private score: number = 0
-
-    constructor(public readonly first: string, public last: string) {
-        this.secretMethod() // This works! (we can access private from the constructor)
-    }
+    constructor(
+        public readonly first: string,
+        public last: string,
+        private _score: number,
+        protected _region: string = "US"
+        ) {
+            this.secretMethod() // This works! (we can access private from the constructor)
+        }
 
     private secretMethod(): void {
         console.log("Secret method!")
     }
+
+    // fullName will be set as readonly by default
+    get fullName(): string {
+        return `${this.first} ${this.last}`
+    }
+
+    get score(): number {
+        return this._score
+    }
+
+    set score(newScore: number) {
+        if(newScore < 0) {
+            throw new Error ("Score cannot be negative")
+        }
+        this._score = newScore
+    }
 }
 
-const player1 = new Player("Cooper", "dog")
+const player1 = new Player("Cooper", "dog", 0)
 console.log(player1)
 // player1.secretMethod() // Property 'secretMethod' is private and only accessible within class 'Player'.
+player1.score
+// player1.score = -1 // Uncaught Error: Score cannot be negativee
+
+class SuperPlayer extends Player {
+    public isAdmin: boolean = true
+
+    switchToEuRegion() {
+        this._region = "EU"
+    }
+}
