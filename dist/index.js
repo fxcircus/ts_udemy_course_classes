77d3059650b05a19a43c74bceb3abc6680257b63
Roy Daniel <roydanielr@gmail.com>
2023-01-09 20:56:48 -0500

    beginning of class

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,28 @@
 "use strict";
-console.log("Hello world!!!");
+// class Player {
+//     readonly first: string
+//     public readonly last: string
+//     private score: number = 0
+//     constructor(first: string, last: string) {
+//         this.first = first
+//         this.last = last
+//         this.secretMethod() // This works! (we can access private from the constructor)
+//     }
+//     private secretMethod(): void {
+//         console.log("Secret method!")
+//     }
+// }
+class Player {
+    constructor(first, last) {
+        this.first = first;
+        this.last = last;
+        this.score = 0;
+        this.secretMethod(); // This works! (we can access private from the constructor)
+    }
+    secretMethod() {
+        console.log("Secret method!");
+    }
+}
+const player1 = new Player("Cooper", "dog");
+console.log(player1);
+// player1.secretMethod() // Property 'secretMethod' is private and only accessible within class 'Player'.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello world!!!")
+// class Player {
+//     readonly first: string
+//     public readonly last: string
+//     private score: number = 0
+
+//     constructor(first: string, last: string) {
+//         this.first = first
+//         this.last = last
+//         this.secretMethod() // This works! (we can access private from the constructor)
+//     }
+
+//     private secretMethod(): void {
+//         console.log("Secret method!")
+//     }
+// }
+
+class Player {
+    private score: number = 0
+
+    constructor(public readonly first: string, public last: string) {
+        this.secretMethod() // This works! (we can access private from the constructor)
+    }
+
+    private secretMethod(): void {
+        console.log("Secret method!")
+    }
+}
+
+const player1 = new Player("Cooper", "dog")
+console.log(player1)
+// player1.secretMethod() // Property 'secretMethod' is private and only accessible within class 'Player'.
